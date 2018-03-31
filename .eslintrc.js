59f5446debb6fb32c4b758d2d5764412397d0ca5
revathynattamai <revathy.ng@gmail.com>
2018-03-31 15:38:15 +0530

    Adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+      "no-cond-assign": [
+        "error",
+        "always"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ],
+      "indent": [
+        "error",
+        2
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "react/react-in-jsx-scope": "off"
+    },
+    "env": {
+      "browser": true,
+      "node": true,
+      "jasmine": true
+    },
+    "globals": {
+      "ReactDOM": true,
+    },
+  };
